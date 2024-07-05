ae11cafaa5ae439d37f2892d5d03700ddabf8601
Milan Sajiv <78609869+MilanSajiv@users.noreply.github.com>
2024-07-05 11:32:38 +0100

    Update transform.py

--- transform.py
+++ transform.py
@@ -11,23 +11,18 @@ class DataTransformation:
     def convert_term_float(self, term_column):
         self.df[term_column] = self.df[term_column].str.extract('(\d+)', expand=False).astype(float)
         return self.df
-    
     def convert_categorical(self, cat_column):
         self.df[cat_column] = self.df[cat_column].astype('category')
         return self.df
-    
     def convert_cat_to_numerical(self, category_column):
         self.df[category_column] = self.df[category_column].factorize()[0]
         self.df[category_column] = self.df[category_column] + 1
         return self.df
-    
     def convert_to_float(self, column):
         self.df[column] = self.df[column].astype(float)
         return self.df
-
     def drop_column(self, column):
         return self.df.drop(column, axis=1, inplace=True)
-    
     def impute_columns(self, column, impute_type):
         if impute_type == "median":
             self.df[column].fillna(self.df[column].median(), inplace=True)
